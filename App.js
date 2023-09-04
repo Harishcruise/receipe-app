a65a6dd4f59981e1130d47a020e90b93886011fe
Harishcruise <harishbarath2001@gmail.com>
2023-09-04 11:37:41 -0400

    imported fonts

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
+import { useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'Cabinet-Grotesk': require('./assets/fonts/CabinetGrotesk-Regular.otf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text>Open up App.js to start working on your app!</Text>
---
+++ Typography/LabelBlack.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const LabelBlack = () => {
+  return (
+    <>
+      <Text >LabelBlack</Text>
+    </>
+  )
+}
+
+export default LabelBlack
