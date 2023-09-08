db54e2627e146afa2835bfca667dd88a2ba4f7ff
anaikarsaad <anaikarsaad@gmail.com>
2023-09-08 18:06:50 -0400

    otp screen added

--- src/components/InputField/OtpInputField.js
+++ src/components/InputField/OtpInputField.js
@@ -1,11 +1,51 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text,TextInput,Button, Pressable, TouchableOpacity} from 'react-native'
+import React,{useState,useRef} from 'react'
 
 const OtpInputField = () => {
+const[code,setCode]=useState("");
+const[pinReady,setPinReady]=useState(false);
+const [inputContainerisFocused,setInputContainerisFocused]=useState(false);
+const maxCodeLength=4;
+    const textInputref=useRef(null);
+  const handleBlur=()=>{
+    setInputContainerisFocused(false);
+  };
+  
+  const handleOnPress=()=>{
+    setInputContainerisFocused(true);
+    textInputref?.current?.focus();
+    console.log("called");
+  }
+  const codeDigitsArray=new Array(maxCodeLength).fill(0);
+  const toCodeDigitInput=(_value,index)=>{
+    const emptyInputChar=" ";
+    const digit=code[index]||emptyInputChar;
+
+    return (
+      <View key={index} className=" border-b-2 rounded-none min-w-[22%] px-5 border-dark-400">
+        <Text className="text-lg text-center font-bold">{digit}</Text>
+      </View>
+    )
+  }
   return (
-    <View>
-      <Text>OtpInputField</Text>
-    </View>
+    <View className='items-center mt-8'>
+    
+      <Pressable onPress={handleOnPress} className="w-[100%] flex-row justify-around">
+      {codeDigitsArray.map(toCodeDigitInput)}
+      </Pressable>
+      <TextInput value={code} 
+      onChangeText={setCode} 
+      maxLength={maxCodeLength} 
+      keyboardType='number-pad' 
+      returnKeyType='done' 
+      textContentType='oneTimeCode' 
+      ref={textInputref}
+      onBlur={handleBlur}
+      className='w-[100%] bg-light-900 h-[54px] rounded-md text-justify text-lg pl-4 pb-[5px] hidden'
+      />
+   
+    
+  </View>
   )
 }
 
--- src/pages/Onboarding/OtpVerificationPage.js
+++ src/pages/Onboarding/OtpVerificationPage.js
@@ -1,9 +1,9 @@
-import { View, Image, TouchableOpacity} from 'react-native'
+import { View, Image, TouchableOpacity,Text, Alert} from 'react-native'
 import React, { useState } from 'react'
 import {HeaderSecondVariant} from '../../components/Header'
 import { LabelBlack, LabelGray, LabelLink } from '../../Typography'
 import {CustomButton1, CustomButton3} from '../../components/Buttons'
-import { PasswordInputField, TextInputField } from '../../components/InputField'
+import { OtpInputField} from '../../components/InputField'
 import { WrapBackground } from '../../components/Background'
 
 const OtpVerificationPage = ({navigation,route}) => {
@@ -19,21 +19,25 @@ const OtpVerificationPage = ({navigation,route}) => {
     return (
       <WrapBackground>
           <HeaderSecondVariant onPress={()=>navigation.goBack()} />
-  
-          <View style={{gap:36}} className='flex px-4 py-3 w-[100%]'>
+          <View style={{gap:20}} className='flex px-4 py-2 w-[100%]'>
               <View style={{gap:7}} className='flex'>
               <LabelBlack style={{fontSize:24}}>Verification Code</LabelBlack>
-              <LabelGray>Otp is sent to {phoneNumber}</LabelGray>
+              <LabelGray>Otp is sent to  <LabelBlack> {phoneNumber}</LabelBlack></LabelGray>
               </View>
   
-              <View style={{gap:12}} className='flex'>
-                  <TextInputField value={OtpData.otp} onChangeText={onChangeOtp} keyboardType={'numeric'}>Phone Number</TextInputField>
-                  
+              <View style={{gap:5}} className='flex'>
+                <OtpInputField></OtpInputField>
               </View>
-  
+
+              <TouchableOpacity className='flex items-end px-1' onPress={()=>{navigation.navigate('OtpVerificationPage',{
+        phoneNumber:phoneNumber})}}>
+                      <LabelGray>Resend OTP!</LabelGray>
+                        
+                    
+                    </TouchableOpacity>
               <View style={{gap:20}} className='flex items-center'>
                   <View style={{gap:8}}  className='flex flex-row justify-between'>
-                      <CustomButton1 style={{width:"100%"}} onPress={()=>navigation.navigate('OtpVerificationPage')} >Request OTP</CustomButton1>
+                      <CustomButton1 style={{width:"100%"}} onPress={()=>{Alert.alert("wait for backend")}} >Request OTP</CustomButton1>
                   </View>
   
           
